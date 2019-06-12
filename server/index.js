b1abbb642212709e42769335771b2d9dc7110f11
Glitch (sveltejs-template-2) <none>
2019-06-12 20:21:11 +0000

    💆🌧 Checkpoint ./server/index.js:234254/11

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express()
 const port = process.env.PORT
 
-app.use(express.static())
+app.use(express.static('../public'));
 
 app.get('/', (req, res) => {
   console.log(process.env.HOME + '/public/index.html');
