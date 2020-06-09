a54758abdc604d0e40d02dc5b9328d012d678b86
Glitch (sveltejs-template-starter) <none>
2020-06-09 16:45:16 +0000

    🐔🌗 Checkpoint ./src/App.svelte:3066/832 ./public/global.css:3066/847

--- src/App.svelte
+++ src/App.svelte
@@ -8,17 +8,26 @@
 	}
 </style>
 
-<h1>Hello {name}!</h1>
-
 <form>
   <ol>
-    <li>
+    <li tabindex="0">
       Springtime for Whiskey Sour
       <ul>
-        <li>¾oz aquafaba</li>
-        <li>1oz lemon juice</li>
-        <li>1oz bay leaf syrup/li>
-        <li>¾oz aquafaba</li>
+        <li tabindex="0">¾oz aquafaba</li>
+        <li tabindex="0">1oz lemon juice</li>
+        <li tabindex="0">1oz bay leaf syrup</li>
+        <li tabindex="0">5 leaves marmajam</li>
+        <li tabindex="0">2oz bourbon</li>
+      </ul>
+    </li>
+    <li tabindex="0">
+      Bramble
+      <ul>
+        <li tabindex="0">2 hood strawberries</li>
+        <li tabindex="0">0.5oz bay leaf syrup</li>
+        <li tabindex="0">1/2 dropper grapefruit bitters</li>
+        <li tabindex="0">Pinch sea salt</li>
+        <li tabindex="0">2oz bourbon</li>
       </ul>
     </li>
   </ol>
