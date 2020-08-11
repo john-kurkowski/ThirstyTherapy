d72fbce5fe8e5371a522fbf008ce0d324cacba73
John Kurkowski <john.kurkowski@gmail.com>
2020-08-10 20:40:02 -0700

    Extract dynamic content into own component

--- src/App.svelte
+++ src/App.svelte
@@ -1,87 +1,10 @@
 <script>
+  import Episode from "./Episode.svelte";
   import Tailwindcss from "./Tailwindcss.svelte";
 </script>
-
-<style>
-  .font-display {
-    @apply text-3xl;
-  }
-
-  label {
-    @apply ml-3;
-  }
-
-  label:hover {
-    cursor: pointer;
-  }
-
-  input:checked + label {
-    color: lightgray;
-    text-decoration: line-through;
-  }
-</style>
 
 <Tailwindcss />
 
 <form class="px-4 py-2">
-  <ol>
-    <li tabindex="0">
-      <div class="flex items-center">
-        <input type="checkbox" />
-        <label class="font-display">Springtime for Whiskey Sour</label>
-      </div>
-
-      <ul class="ml-4">
-        <li tabindex="0">
-          <input type="checkbox" />
-          <label>¾oz aquafaba</label>
-        </li>
-        <li tabindex="0">
-          <input type="checkbox" />
-          <label>1oz lemon juice</label>
-        </li>
-        <li tabindex="0">
-          <input type="checkbox" />
-          <label>1oz bay leaf syrup</label>
-        </li>
-        <li tabindex="0">
-          <input type="checkbox" />
-          <label>5 leaves marjoram</label>
-        </li>
-        <li tabindex="0">
-          <input type="checkbox" />
-          <label>2oz bourbon</label>
-        </li>
-      </ul>
-    </li>
-    <li tabindex="0">
-      <div class="flex items-center">
-        <input type="checkbox" />
-        <label class="font-display">Bramble</label>
-      </div>
-
-      <ul class="ml-4">
-        <li tabindex="0">
-          <input type="checkbox" />
-          <label>2 hood strawberries</label>
-        </li>
-        <li tabindex="0">
-          <input type="checkbox" />
-          <label>0.5oz bay leaf syrup</label>
-        </li>
-        <li tabindex="0">
-          <input type="checkbox" />
-          <label>1/2 dropper grapefruit bitters</label>
-        </li>
-        <li tabindex="0">
-          <input type="checkbox" />
-          <label>Pinch sea salt</label>
-        </li>
-        <li tabindex="0">
-          <input type="checkbox" />
-          <label>2oz bourbon</label>
-        </li>
-      </ul>
-    </li>
-  </ol>
+  <Episode />
 </form>
