e16036d4d3df9685447e8ad7611a120a32cbbeee
John Kurkowski <john.kurkowski@gmail.com>
2020-12-19 09:23:54 -0800

    Increase nav click target size

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -3,21 +3,17 @@
 </script>
 
 <style>
-  a {
+  .tab-title {
     position: relative;
     display: inline-block;
   }
 
-  a:hover::after {
-    @apply bg-gray-300;
-  }
-
-  [aria-current]::after {
+  [aria-current] .tab-title::after {
     @apply bg-purple-600;
   }
 
-  a:hover::after,
-  [aria-current]::after {
+  [aria-current] .tab-title::after,
+  a:hover .tab-title::after {
     bottom: -4px;
     content: "";
     display: block;
@@ -25,36 +21,38 @@
     position: absolute;
     width: 100%;
   }
+
+  a:hover .tab-title::after {
+    @apply bg-gray-300;
+  }
 </style>
 
 <nav class="mb-12">
   <ul class="flex justify-center mb-4">
     <li>
       <a aria-current={segment === undefined ? 'page' : undefined} href=".">
-        home
+        <span class="tab-title">home</span>
       </a>
     </li>
     <li class="ml-6">
       <a
         aria-current={segment === 'classes' ? 'page' : undefined}
         href="classes">
-        classes
+        <span class="tab-title">classes</span>
       </a>
     </li>
 
-    <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
     <li class="ml-6">
       <a
         aria-current={segment === 'livestream' ? 'page' : undefined}
         href="livestream"
         rel="prefetch">
-        livestream
-      </a>
+        <span class="tab-title">livestream</span>
 
-      <span class="badge ml-1">next:
-        <time datetime="2020-12-19T:18:00:00.000Z-08:00">Dec 19</time>
-      </span>
+        <span class="badge ml-1">next:
+          <time datetime="2020-12-19T:18:00:00.000Z-08:00">Dec 19</time>
+        </span>
+      </a>
     </li>
   </ul>
 
