46b536a7fd0e6f3c72b873b4fc8e86d898fe855c
John Kurkowski <john.kurkowski@gmail.com>
2021-01-17 21:35:11 -0800

    Update upcoming

--- src/components/UpcomingIngredients.svelte
+++ src/components/UpcomingIngredients.svelte
@@ -7,37 +7,45 @@
 </style>
 
 <div>
-  <h3 class="mb-4">Jan 16, 2021</h3>
+  <h3 class="mb-4"><del class="opacity-50">Jan 16, 2021</del> ✅</h3>
 
-  <ul class="mb-4">
+  <ul class="mb-4 opacity-50">
     <li class="mb-4">
-      <h4 class="font-bold">Quick Egg Nog</h4>
-      <ul class="list-disc list-inside ml-4">
-        <li>2oz Japanese whiskey</li>
-        <li>.5oz Yellow Chartreuse</li>
-        <li>1oz clover honey syrup (3:1)</li>
-        <li>1tsp turmeric</li>
-        <li>1 dash cardamom bitters</li>
-        <li>1 whole egg</li>
-      </ul>
+      <details>
+        <summary>
+          <h4 class="font-bold inline-block">Quick Egg Nog</h4>
+        </summary>
+        <ul class="list-disc list-inside ml-4">
+          <li>2oz Japanese whiskey</li>
+          <li>.5oz Yellow Chartreuse</li>
+          <li>1oz clover honey syrup (3:1)</li>
+          <li>1tsp turmeric</li>
+          <li>1 dash cardamom bitters</li>
+          <li>1 whole egg</li>
+        </ul>
+      </details>
     </li>
     <li class="mb-4">
-      <h4 class="font-bold">Tom & Jerry</h4>
-      <ul class="list-disc list-inside ml-4">
-        <li>
-          2oz Tom & Jerry batter
-          <div class="ml-4">
-            (<a
-              class="underline"
-              href="https://drinkboy.com/pdf/TomAndJerry.pdf">Audrey Saunders's
-              recipe</a>)
-          </div>
-        </li>
-        <li>1oz aged rum</li>
-        <li>1oz cognac</li>
-        <li>6oz boiling milk</li>
-        <li>Nutmeg</li>
-      </ul>
+      <details>
+        <summary>
+          <h4 class="font-bold inline-block">Tom & Jerry</h4>
+        </summary>
+        <ul class="list-disc list-inside ml-4">
+          <li>
+            2oz Tom & Jerry batter
+            <div class="ml-4">
+              (<a
+                class="underline"
+                href="https://drinkboy.com/pdf/TomAndJerry.pdf">Audrey
+                Saunders's recipe</a>)
+            </div>
+          </li>
+          <li>1oz aged rum</li>
+          <li>1oz cognac</li>
+          <li>6oz boiling milk</li>
+          <li>Nutmeg</li>
+        </ul>
+      </details>
     </li>
   </ul>
 </div>
