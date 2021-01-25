e5c3673dfdada7ac93bb7d6a709d66f08e09bd30
John Kurkowski <john.kurkowski@gmail.com>
2021-01-24 16:23:28 -0800

    Update upcoming

--- src/components/UpcomingIngredients.svelte
+++ src/components/UpcomingIngredients.svelte
@@ -7,58 +7,38 @@
 </style>
 
 <div>
-  <h3 class="mb-4"><del class="opacity-50">Jan 16, 2021</del> ✅</h3>
-
-  <ul class="mb-4 opacity-50">
-    <li class="mb-4">
-      <details>
-        <summary>
-          <h4 class="font-bold inline-block">Quick Egg Nog</h4>
-        </summary>
-        <ul class="list-disc list-inside ml-4">
-          <li>2oz Japanese whiskey</li>
-          <li>.5oz Yellow Chartreuse</li>
-          <li>1oz clover honey syrup (3:1)</li>
-          <li>1tsp turmeric</li>
-          <li>1 dash cardamom bitters</li>
-          <li>1 whole egg</li>
-        </ul>
-      </details>
-    </li>
-    <li class="mb-4">
-      <details>
-        <summary>
-          <h4 class="font-bold inline-block">Tom & Jerry</h4>
-        </summary>
-        <ul class="list-disc list-inside ml-4">
-          <li>
-            2oz Tom & Jerry batter
-            <div class="ml-4">
-              (<a
-                class="underline"
-                href="https://drinkboy.com/pdf/TomAndJerry.pdf">Audrey
-                Saunders's recipe</a>)
-            </div>
-          </li>
-          <li>1oz aged rum</li>
-          <li>1oz cognac</li>
-          <li>6oz boiling milk</li>
-          <li>Nutmeg</li>
-        </ul>
-      </details>
-    </li>
-  </ul>
-</div>
-
-<div>
   <h3 class="mb-4">Jan 30, 2021</h3>
 
   <ul class="mb-4">
     <li class="mb-4">
-      <h4 class="font-bold">Spicy hot drink to warm you up</h4>
+      <h4 class="font-bold">Some Like It Hot</h4>
+      <ul class="list-disc list-outside ml-4">
+        <li>Scotch</li>
+        <li>Espresso / coffee / reheated cold brew</li>
+        <li>Scotch bonnet syrup</li>
+        <li>Hot water</li>
+        <li>Whipped cream</li>
+      </ul>
     </li>
     <li class="mb-4">
-      <h4 class="font-bold">Spicy iced drink to warm you up</h4>
+      <h4 class="font-bold">Some Like It Cold</h4>
+      <ul class="list-disc list-outside ml-4">
+        <li>Scotch</li>
+        <li>Leftover espresso / coffee / cold brew</li>
+        <li>Scotch bonnet syrup</li>
+        <li>Whipped cream</li>
+      </ul>
+    </li>
+    <li class="mb-4">
+      <h4 class="font-bold">Some Like It Highball</h4>
+      <ul class="list-disc list-outside ml-4">
+        <li>Gin</li>
+        <li>Scotch bonnet syrup</li>
+        <li>Rose-infused soju</li>
+        <li>Szechuan peppercorn bitters</li>
+        <li>Soda</li>
+        <li>Lemon peel</li>
+      </ul>
     </li>
   </ul>
 </div>
@@ -68,7 +48,7 @@
 
   <ul class="mb-4">
     <li class="mb-4">
-      <h4 class="font-bold">NA drinks</h4>
+      <h4 class="font-bold">Non-alcoholic drinks</h4>
     </li>
   </ul>
 </div>
