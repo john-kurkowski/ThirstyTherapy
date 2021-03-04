9173653742106d583f0c5eaec97e77aee8ed6264
John Kurkowski <john.kurkowski@gmail.com>
2021-03-03 18:55:46 -0800

    Run `svelte-check` per project folder
    It does not expect to be run with a single file argument. It does not expect to be run over the entire monorepo.

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,29 @@
+const path = require("path");
+const pkgUp = require("pkg-up");
+
 module.exports = {
   "*.css": ["stylelint --fix", "prettier --write"],
-  "*.svelte": [
-    "eslint --cache --fix --max-warnings=0",
-    "svelte-check",
-    "prettier --write",
-  ],
+
+  "*.svelte": (filenames) => {
+    const distintProjectFolders = [
+      ...new Set(
+        filenames.map((filename) =>
+          path.dirname(pkgUp.sync({ cwd: path.dirname(filename) }))
+        )
+      ),
+    ];
+
+    const svelteChecks = distintProjectFolders.map(
+      (projectFolder) => `svelte-check --workspace ${projectFolder}`
+    );
+
+    return [
+      `eslint --cache --fix --max-warnings=0 ${filenames.join(" ")}`,
+      ...svelteChecks,
+      `prettier --write ${filenames.join(" ")}`,
+    ];
+  },
+
   "*.{js,ts,tsx}": [
     "eslint --cache --fix --max-warnings=0",
     "prettier --write",
