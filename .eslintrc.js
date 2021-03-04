eb7d973bd02f48cc4e905e8db37f92fda3fff38f
John Kurkowski <john.kurkowski@gmail.com>
2021-03-03 18:54:23 -0800

    Lint root dotfiles

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: "module",
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ["eslint:recommended"],
+};
--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,12 @@
 module.exports = {
   "*.css": ["stylelint --fix", "prettier --write"],
-  "*.svelte": ["eslint --cache --fix --max-warnings=0", "svelte-check", "prettier --write"],
-  "*.{ts,tsx}": ["eslint --cache --fix --max-warnings=0", "prettier --write"],
-}
+  "*.svelte": [
+    "eslint --cache --fix --max-warnings=0",
+    "svelte-check",
+    "prettier --write",
+  ],
+  "*.{js,ts,tsx}": [
+    "eslint --cache --fix --max-warnings=0",
+    "prettier --write",
+  ],
+};
