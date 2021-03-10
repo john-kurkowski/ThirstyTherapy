f6bb2fbbfc83d62089a2dbfde60d150264bc5f11
John Kurkowski <john.kurkowski@gmail.com>
2021-03-10 13:35:25 -0800

    Add Twitch client secret form
    * Set cookie on form input change

--- apps/browser-sources/src/stores.js
+++ apps/browser-sources/src/stores.js
@@ -1,3 +1,32 @@
 import { writable } from "svelte/store";
 
 export const pageName = writable("");
+
+export const twitchClientSecret = (function writableCookie() {
+  const cookieName = "ttTCS";
+
+  const existingValue =
+    (new RegExp(`${cookieName}=([^;]+)`, "g").exec(document.cookie) || [])[1] ||
+    "";
+
+  const { set, subscribe } = writable(existingValue);
+
+  const oneMonth = 1000 * 60 * 60 * 24 * 30;
+  const expires = new Date(Date.now() + oneMonth);
+
+  return {
+    set(value) {
+      document.cookie = [
+        `${cookieName}=${value}`,
+        "path=/",
+        "samesite=strict",
+        "secure",
+        `expires=${expires.toUTCString()}`,
+      ].join("; ");
+
+      set(...arguments);
+    },
+
+    subscribe,
+  };
+})();
