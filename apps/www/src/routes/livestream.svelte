69e41a244f140f1f29d4fdf4faa06897739be492
John Kurkowski <john.kurkowski@gmail.com>
2021-03-10 19:12:41 -0800

    Fix svelte-check error on Tailwind at-rules

--- apps/www/src/routes/livestream.svelte
+++ apps/www/src/routes/livestream.svelte
@@ -2,7 +2,107 @@
   import UpcomingIngredients from "../components/UpcomingIngredients.svelte";
 </script>
 
-<style>
+<svelte:head>
+  <title>Livestream | Thirsty Therapy</title>
+  <meta
+    property="og:description"
+    content="Welcome in, folks, to the socially safe cocktail bar! Watch on Twitch.
+                  Follow along to put fresh spins on your favorite spirits. You are #worthyofgooddrinks."
+  />
+  <meta property="og:image" content="https://thirstytherapy.com/stream.png" />
+  <meta property="og:title" content="Livestream | Thirsty Therapy" />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://thirstytherapy.com/livestream" />
+</svelte:head>
+
+<div class="text-left">
+  <div class="max-w-2xl mx-auto">
+    <blockquote class="border-l-2 border-purple-600 italic mb-6 pl-2">
+      Welcome in, folks, to the socially safe cocktail bar! Sit at the bar with
+      Billy the master bartender and alchemist, and his apprentice John. Follow
+      along to put fresh spins on your favorite spirits. You are worthy of good
+      drinks.
+    </blockquote>
+
+    <p class="mb-6 text-center text-lg">
+      <a class="align-middle button" href="https://twitch.tv/thirstytherapy">
+        Watch on Twitch
+        <img
+          class="inline-block ml-2"
+          src="./iconmonstr-twitch-1.svg"
+          alt="Twitch logo"
+        />
+      </a>
+    </p>
+
+    <p class="mb-6">
+      We stream every other Saturday, 6-8pm Pacific,
+      <a class="underline" href="https://twitch.tv/thirstytherapy">on Twitch</a
+      >.
+    </p>
+  </div>
+
+  <div class="cards justify-between max-w-2xl mb-6 mx-auto xl:max-w-5xl">
+    <div class="card mb-6">
+      <header class="mb-4">
+        <h2>Upcoming Ingredients</h2>
+      </header>
+
+      <UpcomingIngredients />
+    </div>
+
+    <div class="card mb-6">
+      <header class="mb-4">
+        <h2 class="mb-2">About Billy</h2>
+        <img
+          alt="Billy the bartender, smiling"
+          class="w-full"
+          src="./billy.png"
+        />
+      </header>
+      <p class="mb-4">
+        Billy is a professional bartender in Portland, OR, USA. He's obsessed
+        with hospitality and alchemy, applying culinary techniques to his drinks
+        and making his own bitters.
+      </p>
+    </div>
+
+    <div class="card mb-6">
+      <header class="mb-4">
+        <h2 class="mb-2">About John</h2>
+        <img alt="John the barback, thinking" class="w-full" src="./john.png" />
+      </header>
+      <p class="mb-4">
+        John is Billy's long time guest sitting at the bar. One day John asked,
+        how to take his home bartending to the next level? Billy told him, put
+        on the apron, get behind the bar.
+      </p>
+    </div>
+
+    <div class="card mb-6">
+      <header class="mb-4">
+        <h2 class="mb-2">More</h2>
+        <img
+          alt="Screenshot of Billy and John bartending remotely during a 2020 livestream"
+          src="./stream.png"
+        />
+      </header>
+
+      <ul class="list-disc list-inside mb-4">
+        <li>
+          <a
+            class="underline"
+            href="https://www.youtube.com/channel/UC_t71znatwtjEPL3i1MiutA"
+          >
+            YouTube archive
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<style lang="postcss">
   .button {
     @apply bg-pink-100;
     @apply border-2;
@@ -46,97 +146,3 @@
     @apply font-display;
   }
 </style>
-
-<svelte:head>
-  <title>Livestream | Thirsty Therapy</title>
-  <meta
-    property="og:description"
-    content="Welcome in, folks, to the socially safe cocktail bar! Watch on Twitch.
-                  Follow along to put fresh spins on your favorite spirits. You are #worthyofgooddrinks." />
-  <meta property="og:image" content="https://thirstytherapy.com/stream.png" />
-  <meta property="og:title" content="Livestream | Thirsty Therapy" />
-  <meta property="og:type" content="website" />
-  <meta property="og:url" content="https://thirstytherapy.com/livestream" />
-</svelte:head>
-
-<div class="text-left">
-  <div class="max-w-2xl mx-auto">
-    <blockquote class="border-l-2 border-purple-600 italic mb-6 pl-2">
-      Welcome in, folks, to the socially safe cocktail bar! Sit at the bar with
-      Billy the master bartender and alchemist, and his apprentice John. Follow
-      along to put fresh spins on your favorite spirits. You are worthy of good
-      drinks.
-    </blockquote>
-
-    <p class="mb-6 text-center text-lg">
-      <a class="align-middle button" href="https://twitch.tv/thirstytherapy">
-        Watch on Twitch
-        <img
-          class="inline-block ml-2"
-          src="./iconmonstr-twitch-1.svg"
-          alt="Twitch logo" />
-      </a>
-    </p>
-
-    <p class="mb-6">
-      We stream every other Saturday, 6-8pm Pacific,
-      <a class="underline" href="https://twitch.tv/thirstytherapy">on Twitch</a>.
-    </p>
-  </div>
-
-  <div class="cards justify-between max-w-2xl mb-6 mx-auto xl:max-w-5xl">
-    <div class="card mb-6">
-      <header class="mb-4">
-        <h2>Upcoming Ingredients</h2>
-      </header>
-
-      <UpcomingIngredients />
-    </div>
-
-    <div class="card mb-6">
-      <header class="mb-4">
-        <h2 class="mb-2">About Billy</h2>
-        <img
-          alt="Billy the bartender, smiling"
-          class="w-full"
-          src="./billy.png" />
-      </header>
-      <p class="mb-4">
-        Billy is a professional bartender in Portland, OR, USA. He's obsessed
-        with hospitality and alchemy, applying culinary techniques to his drinks
-        and making his own bitters.
-      </p>
-    </div>
-
-    <div class="card mb-6">
-      <header class="mb-4">
-        <h2 class="mb-2">About John</h2>
-        <img alt="John the barback, thinking" class="w-full" src="./john.png" />
-      </header>
-      <p class="mb-4">
-        John is Billy's long time guest sitting at the bar. One day John asked,
-        how to take his home bartending to the next level? Billy told him, put
-        on the apron, get behind the bar.
-      </p>
-    </div>
-
-    <div class="card mb-6">
-      <header class="mb-4">
-        <h2 class="mb-2">More</h2>
-        <img
-          alt="Screenshot of Billy and John bartending remotely during a 2020 livestream"
-          src="./stream.png" />
-      </header>
-
-      <ul class="list-disc list-inside mb-4">
-        <li>
-          <a
-            class="underline"
-            href="https://www.youtube.com/channel/UC_t71znatwtjEPL3i1MiutA">
-            YouTube archive
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</div>
