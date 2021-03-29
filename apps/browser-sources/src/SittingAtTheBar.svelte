32acb19250eea59b90719b626b0134662e3e04f1
John Kurkowski <john.kurkowski@gmail.com>
2021-03-28 17:15:45 -0700

    Align right

--- apps/browser-sources/src/SittingAtTheBar.svelte
+++ apps/browser-sources/src/SittingAtTheBar.svelte
@@ -47,43 +47,45 @@
   export const location = "";
 </script>
 
-{#await fetchData}
-  <div class="broadcast-bubble">
-    <Loader />
-  </div>
-{:then data}
-  <div class="broadcast-bubble inline-block">
-    <h2 class="text-tn">Sitting at the bar</h2>
+<div class="align-top inline-flex justify-end w-full">
+  {#await fetchData}
+    <div class="broadcast-bubble">
+      <Loader />
+    </div>
+  {:then data}
+    <div class="broadcast-bubble">
+      <h2 class="text-tn">Sitting at the bar</h2>
 
-    <ol class="flex mt-1">
-      {#each data as entry, i}
-        <li class="flex items-center text-xs {i > 0 && 'ml-4'}">
-          <span aria-label="Drinking a cocktail" role="img">
-            {COCKTAIL_ICONS[i % COCKTAIL_ICONS.length]}
-          </span>
+      <ol class="flex mt-1">
+        {#each data as entry, i}
+          <li class="flex items-center text-xs {i > 0 && 'ml-4'}">
+            <span aria-label="Drinking a cocktail" role="img">
+              {COCKTAIL_ICONS[i % COCKTAIL_ICONS.length]}
+            </span>
 
-          <div class="flex flex-col items-center ml-1">
-            <!-- svelte-ignore a11y-missing-attribute -->
-            <!-- The text below it is sufficient. -->
-            <img class="rounded-full w-4" src={entry.profile_image_url} />
-            <p>{entry.display_name}</p>
-          </div>
-        </li>
-      {/each}
-    </ol>
-  </div>
-{:catch error}
-  <div class="broadcast-bubble inline-block text-tn">
-    <p>
-      No one at the bar! 😭 Are your
-      <Link to="/admin">
-        <span class="underline">admin settings</span>
-      </Link>
-      correct?
-    </p>
-    <code class="text-red-600">{error.message}</code>
-  </div>
-{/await}
+            <div class="flex flex-col items-center ml-1">
+              <!-- svelte-ignore a11y-missing-attribute -->
+              <!-- The text below it is sufficient. -->
+              <img class="rounded-full w-4" src={entry.profile_image_url} />
+              <p>{entry.display_name}</p>
+            </div>
+          </li>
+        {/each}
+      </ol>
+    </div>
+  {:catch error}
+    <div class="broadcast-bubble text-tn">
+      <p>
+        No one at the bar! 😭 Are your
+        <Link to="/admin">
+          <span class="underline">admin settings</span>
+        </Link>
+        correct?
+      </p>
+      <code class="text-red-600">{error.message}</code>
+    </div>
+  {/await}
+</div>
 
 <style>
   .text-tn {
