2912ab9ed2831ca9aca7ec81cb12c322108df59c
John Kurkowski <john.kurkowski@gmail.com>
2021-05-04 21:25:22 -0700

    Recenter marketing scene
    Was trying to add a 3rd line. That was too much. But it entailed these other improvements.

--- apps/browser-sources/src/SittingAtTheBar/Scene1.svelte
+++ apps/browser-sources/src/SittingAtTheBar/Scene1.svelte
@@ -1,51 +1,59 @@
 <div class="inline-flex justify-end w-full">
-  <div class="broadcast-bubble flex items-center">
+  <div class="broadcast-bubble flex justify-between items-center scene-line">
     <p class="font-display">Thirsty Therapy</p>
     <img
       alt=""
-      class="ml-4 my-1 rounded-full w-8"
+      class="my-1 rounded-full w-8"
       src="https://static-cdn.jtvnw.net/jtv_user_pictures/62f1dd2d-465d-4099-aa03-475678b920c5-profile_image-300x300.png"
     />
-    <p class="ml-4">
+    <p>
       <code>#worthyofgooddrinks</code>
     </p>
   </div>
 </div>
 
 <div class="inline-flex justify-end mt-1 text-sm w-full">
-  <span class="broadcast-bubble inline-block"> thirstytherapy.com </span>
+  <div class="flex justify-evenly scene-line">
+    <span class="broadcast-bubble inline-block"> thirstytherapy.com </span>
 
-  <span class="broadcast-bubble inline-flex items-center ml-4">
-    <a href="https://instagram.com/thirstytherapy">
-      <img
-        alt="Instagram"
-        class="inline-block"
-        src="/iconmonstr-instagram-11.svg"
-        title="Instagram"
-      />
-    </a>
+    <span class="broadcast-bubble inline-flex items-center">
+      <a href="https://instagram.com/thirstytherapy">
+        <img
+          alt="Instagram"
+          class="inline-block"
+          src="/iconmonstr-instagram-11.svg"
+          title="Instagram"
+        />
+      </a>
 
-    <a href="https://paypal.me/thirstytherapy">
-      <img
-        alt="PayPal"
-        class="inline-block ml-2"
-        height="24"
-        src="/iconmonstr-paypal-1.svg"
-        title="PayPal"
-      />
-    </a>
+      <a href="https://paypal.me/thirstytherapy">
+        <img
+          alt="PayPal"
+          class="inline-block ml-2"
+          height="24"
+          src="/iconmonstr-paypal-1.svg"
+          title="PayPal"
+        />
+      </a>
 
-    <a href="https://venmo.com/thirstytherapy">
-      <img
-        alt="Venmo"
-        class="inline-block ml-2"
-        height="24"
-        src="/venmo-icon.svg"
-        title="Venmo"
-        width="24"
-      />
-    </a>
+      <a href="https://venmo.com/thirstytherapy">
+        <img
+          alt="Venmo"
+          class="inline-block ml-2"
+          height="24"
+          src="/venmo-icon.svg"
+          title="Venmo"
+          width="24"
+        />
+      </a>
 
-    <span class="inline-block ml-4">@ThirstyTherapy</span>
-  </span>
+      <span class="inline-block ml-4">@ThirstyTherapy</span>
+    </span>
+  </div>
 </div>
+
+<style>
+  .scene-line {
+    width: 23rem;
+  }
+</style>
