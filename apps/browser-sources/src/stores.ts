ccd036d8971cf231a470ad0be18988ae3a42a04d
John Kurkowski <john.kurkowski@gmail.com>
2021-12-09 22:46:02 -0800

    Convert module to TypeScript
    * Document methods

--- apps/browser-sources/src/stores.ts
+++ apps/browser-sources/src/stores.ts
@@ -1,4 +1,4 @@
-import { derived, writable } from "svelte/store";
+import { Readable, Writable, derived, writable } from "svelte/store";
 
 export const TWITCH_CLIENT_ID = "yrcihot9pxnayk7czuvy5jplj1r7yd";
 
@@ -9,18 +9,19 @@ export const cmsManagementAccessToken = writableCookie("ttCMSMAT");
 export const twitchClientSecret = writableCookie("ttTCS");
 
 /**
- * Given the user set a client secret, use it to get an access token, and save
- * it in a cookie.
+ * Given the user set a client secret, use it to get an access token. Saves it
+ * in a cookie. This is not writable to the caller, because this function
+ * handles writing automatically.
  *
  * This is derived only truly on first fetch. As long as its cookie is set,
  * subsequent changes to the upstream store will be ignored; they will not be
  * derived. It's an edge case. To truly derive, this cookie needs to know what
  * upstream store value it was made from…?
  */
-export const twitchAccessToken = derived(
+export const twitchAccessToken: Readable<string | Error> = derived(
   twitchClientSecret,
 
-  async function callback($twitchClientSecret, set) {
+  function callback($twitchClientSecret, set) {
     const cookieName = "ttTAT";
 
     const existingValue = readCookieValue(cookieName);
@@ -39,45 +40,64 @@ export const twitchAccessToken = derived(
       .map(([k, v]) => `${k}=${v}`)
       .join("&")}`;
     const url = `${host}/oauth2/token${qs}`;
-    const resp = await fetch(url, { method: "POST" });
-    const result = await resp.json();
+    (async function () {
+      const resp = await fetch(url, { method: "POST" });
+      const result = await resp.json();
 
-    if (!resp.ok) {
-      set(new Error(result.message));
-      return;
-    }
+      if (!resp.ok) {
+        set(new Error(result.message));
+        return;
+      }
 
-    writeCookie(cookieName, result.access_token);
-    set(result.access_token);
+      writeCookie(cookieName, result.access_token);
+      set(result.access_token);
+    })();
   }
 );
 
-function readQueryParamValue(cookieName) {
+/**
+ * Helper to read a cookie name as a query string parameter in the current URL
+ * of the page.
+ *
+ * Cookies maintained in this file can also be read from query string params.
+ */
+function readQueryParamValue(cookieName: string): string | null {
   const urlParams = new URLSearchParams(window.location.search);
   return urlParams.get(cookieName);
 }
 
-function readCookieValue(cookieName) {
+/**
+ * Helper to read a cookie value.
+ */
+function readCookieValue(cookieName: string): string | undefined {
   return (new RegExp(`${cookieName}=([^;]+)`, "g").exec(document.cookie) ||
     [])[1];
 }
 
-function writableCookie(cookieName) {
+/**
+ * Svelte store object that persists to a cookie. Read initially from that same
+ * cookie or query string parameters.
+ */
+function writableCookie(cookieName: string): Writable<string> {
   const initialValue =
     readQueryParamValue(cookieName) || readCookieValue(cookieName);
-  const { set, subscribe } = writable(initialValue);
+  const { set, subscribe, update } = writable(initialValue);
 
   return {
-    set(value) {
+    set(value: string) {
       writeCookie(cookieName, value);
-      set(...arguments);
+      set(value);
     },
 
     subscribe,
+    update,
   };
 }
 
-function writeCookie(cookieName, value) {
+/**
+ * Helper to write a secure cookie with this app's preferred expiry.
+ */
+function writeCookie(cookieName: string, value: string): void {
   const oneMonth = 1000 * 60 * 60 * 24 * 30;
   const expires = new Date(Date.now() + oneMonth);
 
