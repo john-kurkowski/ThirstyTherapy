257edbb2d00fd0c4e8fee10e846c17e407da0189
John Kurkowski <john.kurkowski@gmail.com>
2022-02-26 11:09:35 -0800

    Parse OLCC CSV
    * Emit parsing errors
    * Group duplicate products by size
      * Eventually, we'll dedupe because we only care about the biggest

---
+++ apps/database/src/bin/import-olcc.rs
@@ -0,0 +1,65 @@
+use itertools::Itertools;
+use serde::Deserialize;
+use strum::Display;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct OlccItem {
+    age: String,
+    description: String,
+    #[serde(rename = "New Item Code")]
+    new_item_code: u64,
+    proof: f32,
+    size: OlccSize,
+    #[serde(rename = "Unit Price")]
+    unit_price: f32,
+}
+
+#[derive(Copy, Clone, Debug, Deserialize, Display)]
+enum OlccSize {
+    #[serde(rename = "1.75 L")]
+    #[strum(serialize = "1.75 L")]
+    OnePointSevenFiveLiter,
+    #[serde(rename = "200 ML")]
+    #[strum(serialize = "200 ML")]
+    TwoHundredMl,
+    #[serde(rename = "375 ML")]
+    #[strum(serialize = "375 ML")]
+    ThreeSevenFiveMl,
+    #[serde(rename = "50 ML")]
+    #[strum(serialize = "50 ML")]
+    FiftyMl,
+    #[serde(rename = "750 ML")]
+    #[strum(serialize = "750 ML")]
+    SevenFiftyMl,
+    #[serde(rename = "LITER")]
+    #[strum(serialize = "LITER")]
+    Liter,
+}
+
+fn main() {
+    let mut reader = csv::Reader::from_reader(std::io::stdin());
+    let (successes, failures): (Vec<_>, Vec<_>) = reader
+        .deserialize()
+        .map(|result| {
+            let record: Result<OlccItem, csv::Error> = result;
+            record
+        })
+        .partition_result();
+
+    let is_success = failures.is_empty();
+    for failure in failures {
+        println!("{failure}");
+    }
+
+    let groups = successes
+        .iter()
+        .group_by(|record| record.description.clone());
+
+    for (description, group) in &groups {
+        let sizes = group.map(|record| record.size).join(", ");
+        println!("{}: {}", description, sizes);
+    }
+
+    std::process::exit(if is_success { 0 } else { 1 })
+}
