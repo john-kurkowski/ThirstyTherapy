b82d1e24f3cbb608aad79c155a0c8550220db8f5
John Kurkowski <john.kurkowski@gmail.com>
2023-04-03 16:33:33 -0700

    Allow linting outside of commit hook
    * Document lint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,6 +2,12 @@ const path = require("path");
 const pkgUp = require("pkg-up");
 const { ESLint } = require("eslint");
 
+/**
+ * Honor ignored ESLint files in lint-staged.
+ *
+ * Avoids generating warnings avoid ignored files in CI, which Rush would treat
+ * as errors.
+ */
 async function ignoredEslintFiles(files) {
   const eslint = new ESLint();
   const isIgnored = await Promise.all(
@@ -12,15 +18,39 @@ async function ignoredEslintFiles(files) {
   return files.filter((_, i) => !isIgnored[i]);
 }
 
-module.exports = {
-  "*.css": ["stylelint --fix", "prettier --write"],
+/**
+ * Find the path to a command in the subproject responsible for linting.
+ *
+ * Otherwise, lint-staged might not find the correct executable, when run
+ * outside of the Rush project's context.
+ */
+function bin(cmd) {
+  return path.resolve(
+    __dirname,
+    "common",
+    "autoinstallers",
+    "rush-husky",
+    "node_modules",
+    ".bin",
+    cmd
+  );
+}
 
-  "*.md": ["prettier --write"],
+module.exports = {
+  "*.css": [`${bin("stylelint")} --fix`, `${bin("prettier")} --write`],
+
+  "*.md": [`${bin("prettier")} --write`],
 
   "*.sh": "shellcheck",
 
+  /**
+   * Run typical JS checks, and run `svelte-check` per project folder.
+   *
+   * `svelte-check` does not expect to be run with a single file argument and
+   * it does not expect to be run over the entire monorepo.
+   */
   "*.svelte": (filenames) => {
-    const distintProjectFolders = [
+    const distinctProjectFolders = [
       ...new Set(
         filenames.map((filename) =>
           path.dirname(pkgUp.sync({ cwd: path.dirname(filename) }))
@@ -28,23 +58,23 @@ module.exports = {
       ),
     ];
 
-    const svelteChecks = distintProjectFolders.map(
-      (projectFolder) => `svelte-check --workspace ${projectFolder}`
+    const svelteChecks = distinctProjectFolders.map(
+      (projectFolder) => `${bin("svelte-check")} --workspace ${projectFolder}`
     );
 
     return [
-      `eslint --cache --fix --max-warnings=0 ${filenames.join(" ")}`,
+      `${bin("eslint")} --cache --fix --max-warnings=0 ${filenames.join(" ")}`,
       ...svelteChecks,
-      `prettier --write ${filenames.join(" ")}`,
+      `${bin("prettier")} --write ${filenames.join(" ")}`,
     ];
   },
 
   "*.{js,ts,tsx}": async (files) => {
     return [
-      `eslint --cache --fix --max-warnings=0 ${(
+      `${bin("eslint")} --cache --fix --max-warnings=0 ${(
         await ignoredEslintFiles(files)
       ).join(" ")}`,
-      `prettier --write ${files.join(" ")}`,
+      `${bin("prettier")} --write ${files.join(" ")}`,
     ];
   },
 };
--- README.md
+++ README.md
@@ -18,3 +18,37 @@ overlays.
 2. `rushx` to inspect the subproject's available lifecycle commands, like
    starting a development server.
 3. Run your relevant lifecycle command, such as `rushx start`.
+
+### Test
+
+#### Lint and Typecheck
+
+Lint and typecheck commands are the same across all apps in this project.
+**The checks are run on commit, on staged files.**
+
+The Node.js "app" that performs these checks is a Rush autoinstaller, in
+[`common/autoinstallers/rush-husky/`](common/autoinstallers/rush-husky/). In
+order for the autoinstaller to perform against all files in the project, the
+lint config is in the project root, [`.lintstagedrc.js`](.lintstagedrc.js).
+
+To perform the checks outside of the commit hook, use the full path to tools in
+the autoinstaller directory. For example:
+
+```sh
+common/autoinstallers/rush-husky/node_modules/.bin/eslint --fix my-file.js
+```
+
+Consider running your command only on the subproject you're working on, usually
+in `apps/`. For example, with `lint-staged`'s `--cwd` flag:
+
+```sh
+common/autoinstallers/rush-husky/node_modules/.bin/lint-staged --cwd apps/my-app/
+```
+
+To perform checks on all files regardless of Git status, while `lint-staged`
+doesn't have an `--all-files` flag, there is the `--diff` flag you can abuse.
+For example, linting the last 20 commits worth of changed files:
+
+```sh
+common/autoinstallers/rush-husky/node_modules/.bin/lint-staged --diff @~20
+```
